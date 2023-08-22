169a73bfe742725f2b1da5e1afa9199eb9d32628
Anderson A. Silva <andersonadsilva15@gmail.com>
2023-08-21 21:57:56 -0300

    Adicionando index.js no repositório

---
+++ index.js
@@ -0,0 +1,24 @@
+/*
+Exercício 1
+Resposta:
+a. undefined
+b. null
+c. 11
+d. 0
+e. 19, 19
+f. array[i+6]
+*/
+
+/*
+Exercício 2
+Resposta:
+Subi num ônibus em Marrocos, 1
+*/
+
+/*
+Exercício 3
+Resposta:
+*/
+const nomeDoUsuario = prompt("Qual o seu nome?")
+const emailDoUsuario = prompt("Qual o seu e-mail?")
+console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
