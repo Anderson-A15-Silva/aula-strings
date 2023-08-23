0eda242fec454f3ea4c8bd09aaa89b66822fd412
Anderson A. Silva <andersonadsilva15@gmail.com>
2023-08-22 21:18:36 -0300

    Adicionando nova versão index.js no repositório

--- index.js
+++ index.js
@@ -5,20 +5,73 @@ a. undefined
 b. null
 c. 11
 d. 0
-e. 19, 19
+e. 3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13
 f. array[i+6]
 */
 
 /*
 Exercício 2
 Resposta:
-Subi num ônibus em Marrocos, 1
+SUBI NUM ONIBUS EM MIRROCOS, 27
 */
 
 /*
-Exercício 3
+Exercício 1
 Resposta:
-*/
+
 const nomeDoUsuario = prompt("Qual o seu nome?")
 const emailDoUsuario = prompt("Qual o seu e-mail?")
 console.log(`O e-mail ${emailDoUsuario} foi cadastrado com sucesso. Seja bem-vinda(o), ${nomeDoUsuario}!`)
+*/
+
+/*
+Exercício 2
+Resposta:
+
+const array = ["pizza", "lasanha", "bolo de chocolate", "alaminuta", "sorvete"]
+console.log(array)
+console.log("Essas são as minhas comidas preferidas: ", array)
+
+const desafioComida = prompt("Qual a sua comida preferida?")
+array[1] = desafioComida
+console.log(array)
+*/
+/*
+Exercício 3 (Incompleto)
+Resposta:
+
+let listaDeTarefas = [];
+let TarefasPrimeira = prompt("Insira tarefas que você precisa realizar neste dia. Primeira tarefa:");
+let TarefasSegunda = prompt("Insira tarefas que você precisa realizar neste dia. Segunda tarefa:");
+let TarefasTerceira = prompt("Insira tarefas que você precisa realizar neste dia. Terceira tarefa:");
+
+listaDeTarefas[0] = TarefasPrimeira;
+listaDeTarefas[1] = TarefasSegunda;
+listaDeTarefas[2] = TarefasTerceira;
+console.log(listaDeTarefas);
+
+let deletarElemento = prompt("Digite o índice para remover o elemento do array: ")
+listaDeTarefas.splice(deletarElemento, 1);
+console.log(listaDeTarefas);
+*/
+
+/*
+Desafio 1 (Incompleto)
+Resposta:
+
+let fraseDesafio = prompt("Insira uma frase: ");
+let resultado = fraseDesafio.split(" ");
+console.log(resultado);
+*/
+
+/*
+Desafio 2
+Resposta:
+
+const arrayDesafio = ["Banana", "Morango", "Abacaxi", "Laranja", "Ameixa"]
+const tamanhoArrayDesafio = arrayDesafio.length
+const resultadoDesafio = Number(tamanhoArrayDesafio) - 1
+const indiceAbacaxi = resultadoDesafio
+console.log("Índice do Abacaxi", indiceAbacaxi)
+console.log("Tamanho do Array", tamanhoArrayDesafio)
+*/
